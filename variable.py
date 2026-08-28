7e26d8e6f1783e34668847316c123b11b949487b
Areebabutt224 <areebabutt053@gmail.com>
2026-08-28 09:09:15 -0400

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,26 @@
+#Declaring and Printing Variables
+name="Areeba"
+age=20
+height=3.9
+print(name)
+print(age)
+print(height)
+
+#Multiple Variables in One Line
+x, y, z = 5, 10, 15
+print(x, y, z)
+
+#Assigning Same Value to Multiple Variables
+a = b = c = 100
+print(a, b, c)
+
+#Variable Data Types
+name = "Sara"        
+age = 21              
+height = 5.6           
+is_student = True      
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
