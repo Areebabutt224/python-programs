975714d0628358ebc2cbe0f0f08a72028b68aa25
Areebabutt224 <areebabutt053@gmail.com>
2026-08-28 09:15:33 -0400

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,27 @@
+#Single-Line Comment
+# This is a single-line comment
+print("Hello, World!")
+
+#Comment After Code
+print("Learning Python")  # This prints a message
+
+#Multiple Single-Line Comments
+# This program adds two numbers
+# a and b are the variables used
+a = 5
+b = 10
+print(a + b)
+
+#Multi-Line Comment (Using Multiple #)
+# This is line one of the comment
+# This is line two of the comment
+# This is line three of the comment
+print("Multi-line comment example")
+
+#Multi-Line Comment (Using Triple Quotes)
+"""
+This is a multi-line comment
+written using triple quotes.
+It can span several lines.
+"""
+print("Triple quote comment example")
