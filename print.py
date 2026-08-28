567ab16decf80fb002c9ef608af20cd2ab889437
Areebabutt224 <areebabutt053@gmail.com>
2026-08-28 08:59:19 -0400

    Add files via upload

---
+++ print.py
@@ -0,0 +1,20 @@
+#simple hello program
+print("Hello, welcome to Python programming!")
+
+
+#multiple sentence print
+
+print("Python is easy to learn.")
+print("It is used in AI, web development, and data science.")
+
+#Sum of Two Numbers
+a = 10
+b = 5
+print("Sum:", a + b)
+
+#Subtraction, Multiplication, and Division
+a = 20
+b = 4
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
