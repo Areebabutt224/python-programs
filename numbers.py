e60e81937b336fa8ea631c1c0e5ff4b56ec7d438
Areebabutt224 <areebabutt053@gmail.com>
2026-08-31 12:46:51 -0400

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,38 @@
+#Check Even or Odd
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
+
+    #Check Positive, Negative, or Zero
+    num = int(input("Enter a number: "))
+
+if num > 0:
+    print("Positive number")
+elif num < 0:
+    print("Negative number")
+else:
+    print("Zero")
+
+    #Square and Cube
+    num = int(input("Enter a number: "))
+print("Square:", num * num)
+print("Cube:", num * num * num)
+
+#Sum of Two Numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Sum:", a + b)
+
+#Largest of Two Numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+if a > b:
+    print("Largest:", a)
+else:
+    print("Largest:", b)
+
+    
