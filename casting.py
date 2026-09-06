ace112a070b1dc323232c1acc5f463897fe5691a
Areebabutt224 <areebabutt053@gmail.com>
2026-09-06 11:17:01 -0400

    Add files via upload

---
+++ casting.py
@@ -0,0 +1,11 @@
+#int() —  Mein Convert Karna
+num = int("50")
+print(num)
+
+#float() —  Mein Convert Karna
+num = float("99.99")
+print(num)
+
+#str() —  Mein Convert Karna
+num = str(20)
+print(num)
