fa7dcdaf186e9477704602b9a5a56f367c70614d
Areebabutt224 <areebabutt053@gmail.com>
2026-09-06 11:17:29 -0400

    Add files via upload

---
+++ string.py
@@ -0,0 +1,33 @@
+#Printing a String
+message = "Hello, World!"
+print(message)
+
+#Finding the Length of a String
+name = "Hamza"
+print(len(name))
+
+#String Indexing (Accessing Individual Characters)
+name = "Hamza"
+print(name[0])   
+print(name[1])   
+print(name[4])   
+
+#Negative Indexing (counting from the end):
+name = "Areeba"
+print(name[-1])   
+print(name[-2])   
+
+#Converting to Upper/Lower Case
+text = "Hello World"
+print(text.upper())    
+print(text.lower())     
+
+#Removing Extra Spaces (Strip)
+text = "   Hello   "
+print(text.strip())
+
+#Replacing One Word with Another
+text = "I like Python"
+new_text = text.replace("Python", "Java")
+print(new_text)
+
