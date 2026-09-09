c63dd7d9592ebdb4dc09d7b94d09c8948656d777
Areebabutt224 <areebabutt053@gmail.com>
2026-09-09 11:43:24 -0400

    Add files via upload

---
+++ bolean.py
@@ -0,0 +1,50 @@
+#Basic Boolean Values
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+#Comparison Returns Boolean
+print(10 > 5)
+print(10 < 5)
+
+#Equal and Not Equal
+print(5 == 5)
+print(5 != 3)
+
+#Boolean with if Statement
+age = 18
+if age >= 18:
+    print("You are an adult")
+
+#Boolean with if Statement
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+  #Evaluate Values and Variables
+print(bool("Hello"))
+print(bool(15))
+
+
+#Evaluate two variables
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+
+#bool() Function on String
+print(bool("Hello"))
+print(bool(""))
+
+#bool() Function on Numbers
+print(bool(10))
+print(bool(0))
+
+#bool() Function on List
+print(bool([1, 2, 3]))
+print(bool([]))
